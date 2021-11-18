e135661c21745f6a93722a3d8aa72f455883f5b8
Kelvis <kelvis.alvarez20@gmail.com>
2021-11-17 22:23:51 -0300

    Functionality: Add images to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -42,6 +42,6 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, images: [])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,13 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :task
+  has_many_attached :images
 
   paginates_per 10
   # Validación de soft delete
   acts_as_paranoid
+
+  def comment_image_style(input)
+    images[input].variant(resize: '300x300').processed
+  end
 end
