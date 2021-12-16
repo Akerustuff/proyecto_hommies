32cfdf2a5bf082ab85ee927e9e68001b206d65bc
Kelvis <kelvis.alvarez20@gmail.com>
2021-12-16 00:44:00 -0300

    move rollbar.rb file to config and configure environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
 # Load the Rails application.
-require_relative 'application'
+# require_relative 'application'
 
 # Initialize the Rails application.
-Rails.application.initialize!
+# Rails.application.initialize!
+
+require File.expand_path('../application', __FILE__)
+require File.expand_path('../rollbar', __FILE__)
+
+notify = lambda do |e|
+  Rollbar.with_config(use_async: false) do
+    Rollbar.error(e)
+  end
+rescue StandardError => e
+  Rails.logger.error 'Synchronous Rollbar notification failed.  Sending async to preserve info'
+  Rollbar.error(e)
+end
+
+begin
+  Rails.application.initialize!
+rescue StandardError => e
+  notify.call(e)
+  raise
+end
